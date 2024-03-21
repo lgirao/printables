caed412b61dde00e905ea5d2dd7622e2e3c3e78e
lgirao <lesleygirao@gmail.com>
2024-03-21 13:17:12 -0400

    seeder files

---
+++ backend/seeder/data.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    name: "Frozen Birthday Invitation",
+    price: "2.99",
+    description: "Custom birthday party invitation for Frozen fans.",
+    images: [
+      {
+        public_id: "printables/products/Frozen_birthday_invite_duiref",
+        url: "https://res.cloudinary.com/dmjltkyjb/image/upload/v1711039832/printables/products/Frozen_birthday_invite_duiref.png"
+      }
+    ]
+  },
+  {
+    name: "Frozen Scavenger Hunt",
+    price: "2.99",
+    description: "Frozen scavenger hunt party game. Two game sheets per page. Customizable text.",
+    images: [
+      {
+        public_id: "printables/products/Frozen_Scavenger_Hunt_puidkb",
+        url: "https://res.cloudinary.com/dmjltkyjb/image/upload/v1711039286/printables/products/Frozen_Scavenger_Hunt_puidkb.png"
+      }
+    ]
+  },
+  {
+    name: "Frozen Cupcake Toppers",
+    price: "2.99",
+    description: "Circle shaped cupcake toppers.",
+    images: [
+      {
+        public_id: "printables/products/Frozen_cake_topper_circles_krfjzz",
+        url: "https://res.cloudinary.com/dmjltkyjb/image/upload/v1711039823/printables/products/Frozen_cake_topper_circles_krfjzz.png"
+      }
+    ]
+  }
+]
